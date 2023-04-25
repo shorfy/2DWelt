c82ffef6d6e39ed8898e6d79ffa9aca2e2760ab5
Shayan Callewaert <shayan.callewaert@rub.de>
2023-04-25 22:53:53 +0200

    A

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -13,10 +14,30 @@ public class PlayerMovement : MonoBehaviour
     {
         sr = GetComponent<SpriteRenderer>();
     }
+    void ChangeSpeed()
+    {
+        if (speed == 5f)
+        {
+            speed = 10f;
+            return;
+        }
+        if (speed == 10f)
+        {
+            speed = 15f;
+            return;
+        }
+        if (speed == 15f)
+        {
+            speed = 5f;
+            return;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) ChangeSpeed();
+        
         Vector3 moveVector = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) moveVector.y = 1;
         if (Input.GetKey(KeyCode.A)) moveVector.x = -1;
